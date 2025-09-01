79b904c105d8ca365f0600e08da1d2ff3dabb622
Niyofabrice <nf.fabrice.niyonkuru@gmail.com>
2025-09-01 16:48:32 +0200

    defining environment variables

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Note = require('./models/note')
 
 const app = express()
 
@@ -15,30 +16,14 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger)
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
